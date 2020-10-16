1d6236ca962615e6959ff3ec2891bdbfe3706b33
peter0618 <peterchoi0618@gmail.com>
2020-10-16 22:59:52 +0900

    graphql 환경 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { UserModule } from './module/user/user.module';
+import { join } from 'path';
+
 
 @Module({
-  imports: [],
+  imports: [GraphQLModule.forRoot({
+    typePaths: ["./**/*.graphql"],
+    definitions: {
+      path: join(process.cwd(), 'src/autogen/schema.graphql.ts'),
+      outputAs: 'class',
+    },
+  }), UserModule,],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/autogen/schema.graphql.ts
@@ -0,0 +1,22 @@
+
+/** ------------------------------------------------------
+ * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
+ * -------------------------------------------------------
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+export abstract class IQuery {
+    abstract getAll(): User[] | Promise<User[]>;
+}
+
+export class User {
+    id: string;
+    name: string;
+    age: number;
+}
+
+export class A {
+    id: string;
+    contents: string;
+}
